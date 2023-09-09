712b0b17898be83534a9f1e561be8337fdc4fc63
Lauren Hanson <lohanson33@gmail.com>
2023-09-09 13:34:45 -0500

    Transactional Stored Procedure that adds a new employee with 2 dealerships they will be working for.

---
+++ Scripts/HrTransactionGroup.sql
@@ -0,0 +1,54 @@
+/* 
+Create a SP with a transaction to handle hiring a new employee
+Add a new record for the employee in the Employees table. 
+Add a record to the Dealershipemployees table for the 2 dealerships the new employee will start at
+*/ 
+
+select * from employees 
+	order by employee_id desc
+	
+select * from employeetypes 
+select * from dealerships 
+	order by dealership_id desc
+	
+select * from dealershipemployees 
+	order by dealership_employee_id desc
+	
+
+create or replace procedure NewHireInformation()
+language plpgsql  
+as $$
+declare 
+	NewCustomerId integer; 
+	CurrentTS date; 
+
+begin
+	insert into employees(first_name,last_name,email_address,phone,employee_type_id)
+	values('George','Hanson','george@george.com','6157245692', 7)
+	returning employee_id into NewCustomerId; 
+	
+	commit; 
+
+	CurrentTS = current_date; 
+
+	insert into dealershipemployees(dealership_id,employee_id)
+	values(75, NewCustomerId); 
+	
+	commit; 
+
+	insert into dealershipemployees(dealership_id,employee_id)
+	values(51, NewCustomerId); 
+	
+	
+end; 
+$$; 
+
+call NewHireInformation(); 
+
+
+
+/*
+Create a SP with a transaction to handle an employee leaving. 
+Remove employee record.
+Remove all records associated with the employee w/ dealerships must also be removed
+*/
